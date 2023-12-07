74b16aa96766e54a976ea708c1463a8e3d8b468a
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 18:19:57 +0530

    Update Static.java

--- Static.java
+++ Static.java
@@ -1,18 +1,15 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
-public class Static {
+public class Test1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B =sc.nextInt();
-        int C =A*B;
-        if(A>0 && B>0) 
+        if(A>0 && B>0) {
+        int C = A * B;
         System.out.println(C);
-        else 
+        }
+        else {
         System.out.println("Invaild measurement");
+        }
     }
 }
