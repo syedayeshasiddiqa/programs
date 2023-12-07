6285edb9780dfe0571679551ca4774fe8baf7228
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 18:01:19 +0530

    Update Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -1,17 +1,11 @@
 import java.util.*;
 import java.io.*;
-
 public class DataType {
-    public static void main(String []argh)
-    {
+    public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
+        for(int i=0;i<t;i++){
+            try{
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)
@@ -23,8 +17,7 @@ public class DataType {
                 System.out.println("* long");
 
             }
-            catch(Exception e)
-            {
+            catch(Exception e){
                 System.out.println(sc.next()+" can't be fitted anywhere.");
             }
         }
