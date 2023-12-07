808483a9962a16ace63ee651ed8b0cd178862582
syedayeshasiddiqa <93445468+syedayeshasiddiqa@users.noreply.github.com>
2023-12-07 18:28:42 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,11 @@
 // Chaining Constructors ----> calling one constructor from another constructor
 class Student {
 
-	String name;				 										// 1 byte
-	int rollNo;															// 1 byte
-	String collegeName;													// 1 byte
-	String collegeLocation;       										// 1 byte
-	String dept;														// 1 byte
+	String name;				 										
+	int rollNo;															
+	String collegeName;													
+	String collegeLocation;       										
+	String dept;														
 	String gender;	
 	
 	public Student(String dept, String gender, int rollNo){
@@ -15,26 +15,26 @@ class Student {
 	}
 
 	public Student(String collegeName, String collegeLocation){
-		this("ECE", "Female", 18);
+		this("ECE","Female",8);
 		this.collegeName = collegeName;
 		this.collegeLocation = collegeLocation;
 	}
 
 	public Student(String name){
-		this("Ashoka Women's College", "Dupadu"); // line of code 
+		this("Ashoka Women's Engg clg","Kurnool");
 		this.name = name;
 	}
 }
 
 public class Test{
 	public static void main(String[] args){   
-		Student s1 = new Student("Haritha");
-		System.out.println(s1.name);  // Haritha
+		Student s1 = new Student("Ayesha");
+		System.out.println(s1.name);  // null
 		System.out.println(s1.collegeName); // null
 		System.out.println(s1.collegeLocation); // null
-		System.out.println(s1.rollNo);  // 0
-		System.out.println(s1.gender); // null
-		System.out.println(s1.dept); // null
+		System.out.println(s1.rollNo);  // 8
+		System.out.println(s1.gender); // Female
+		System.out.println(s1.dept); // ECE
 
 	}
 }
